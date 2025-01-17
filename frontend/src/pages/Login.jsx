e8512cc01f85b6e14b4df8839a42423e8722027a
Hira-deep <hira0407v@gmail.com>
2025-01-17 13:17:28 +0530

    modified

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import { Container,Row, Col,Form,FormGroup ,/*Button*/} from 'reactstrap';    
+//import {link} from 'react-router-dom';
+import '../styles/login.css';
+import loginImg from '../assets/images/login.png'
+import userIcon from '../assets/images/user.png'
+
+
 
 const Login = () => {
     return(
-        <div>Login</div>
+        <section>
+            <Container>
+                <Row>
+                    <Col lg='8' className="m-auto">
+                    <div className="login__container d-flex justify-content-between">
+                        <div className="login__img">
+                            <img src={loginImg} alt=""/>
+
+                        </div>
+                        <div className="login__form">
+                        <div className="user">
+                            <img src={userIcon} alt=""/>
+
+                        </div>
+                        <h2>Login</h2>
+                        <Form>
+                            <FormGroup>
+                                <input type='text' placeholder='Email' required id='email'/>
+                            </FormGroup>
+                        </Form>
+                        </div>
+                    </div>
+
+                    
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     )
 }
 
--- frontend/src/services/ServiceList.jsx
+++ frontend/src/services/ServiceList.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ServiceCard from './ServiceCard';
 import { Col } from 'reactstrap';
 
-import weatherImg from '../assets/images/weather.png';
-import guideImg from '../assets/images/guide.png';
-import customizationImg from '../assets/images/customization.png';
+import weatherImg from '../assets/images/weather2.jpg';
+import guideImg from '../assets/images/guide3.jpg';
+import customizationImg from '../assets/images/customization1.jpg';
 
 const servicesData = [
     {
